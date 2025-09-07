5203bd38736ea5a09e13fa050800bbf20067f9ec
vaibhavchau37 <vaibhavchaudhary508@gmail.com>
2025-09-07 12:02:19 +0530

    Updated portfolio with new changes

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,8 @@
 import { Inter } from "next/font/google";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import Footer from "./components/footer";
 import ScrollToTop from "./components/helper/scroll-to-top";
 import Navbar from "./components/navbar";
+import ToastContainerWrapper from "./components/helper/toast-container";
 import "./css/card.scss";
 import "./css/globals.scss";
 const inter = Inter({ subsets: ["latin"] });
@@ -18,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ToastContainer />
+        <ToastContainerWrapper />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
           <Navbar />
           {children}
